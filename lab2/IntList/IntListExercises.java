c856b4d2f800303acb0055e5783ff100706226f6
konakona418 <creeperblin@outlook.com>
2025-02-23 20:16:33 +0800

    feat: lab2 update.

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -55,7 +55,7 @@ public class IntListExercises {
             return false;
         }
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
